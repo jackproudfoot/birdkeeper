3be1fc334ca04693fb2c9f07a7c9949657d2809b
Jack Proudfoot <jackson.proudfoot@gmail.com>
2023-02-07 14:30:10 -0500

    added admin code

--- aviary/server/aviary/admin.py
+++ aviary/server/aviary/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from .models import Flight, Media, FlightMedia, Observation, ObservationMedia, Identification, IdentificationObservation
 
 # Register your models here.
+class FlightAdmin(admin.ModelAdmin):
+    list_display = ('drone', 'start', 'end', 'pilot')
+
+class MediaAdmin(admin.ModelAdmin):
+    list_display = ('file', 'type', 'geo_data')
+
+class FlightMediaAdmin(admin.ModelAdmin):
+    list_display = ('flight_id', 'media_id')
+
+class ObservationAdmin(admin.ModelAdmin):
+    list_display = ('description', 'type')
+
+class ObservationMediaAdmin(admin.ModelAdmin):
+    list_display = ('observation_id', 'media_id')
+
+class IdentificationAdmin(admin.ModelAdmin):
+    list_display = ('source', 'species')
+
+class IdentificationObservationAdmin(admin.ModelAdmin):
+    list_display = ('observation_id', 'identification_id')
+
+admin.site.register(Flight, FlightAdmin)
+admin.site.register(Media, MediaAdmin)
+admin.site.register(FlightMedia, FlightMediaAdmin)
+admin.site.register(Observation, ObservationAdmin)
+admin.site.register(ObservationMedia, ObservationMediaAdmin)
+admin.site.register(Identification, IdentificationAdmin)
+admin.site.register(IdentificationObservation, IdentificationObservationAdmin)
--- aviary/server/aviary/models.py
+++ aviary/server/aviary/models.py
@@ -3,6 +3,9 @@ from django.db import models
 class Users(models.Model):
     name = models.CharField(max_length=20)
 
+    def __str__(self):
+        return self.name
+
 class Flight(models.Model):
     drone = models.CharField(max_length=60)
     start = models.DateTimeField()
