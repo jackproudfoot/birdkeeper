36e890ae32a4c523d71f981039f66ef97e7c97fd
Jack Proudfoot <jackson.proudfoot@gmail.com>
2023-02-07 13:43:15 -0500

    added models

--- aviary/server/app/settings.py
+++ aviary/server/app/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 # Application definition
 
 INSTALLED_APPS = [
+    'aviary.apps.AviaryConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
---
+++ aviary/server/aviary/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+
+class Users(models.Model):
+    name = models.CharField(max_length=20)
+
+class Flight(models.Model):
+    drone = models.CharField(max_length=60)
+    start = models.DateTimeField()
+    end   = models.DateTimeField()
+    pilot = models.ForeignKey(Users, on_delete=models.DO_NOTHING)
+
+class Media(models.Model):
+    file     = models.URLField()
+    type     = models.CharField(max_length=20)
+    geo_data = models.JSONField()
+
+class FlightMedia(models.Model):
+    flight_id = models.ForeignKey(Flight, on_delete=models.CASCADE)
+    media_id  = models.ForeignKey(Media, on_delete=models.CASCADE)
+
+class Observation(models.Model):
+    description = models.TextField()
+    type        = models.CharField(max_length=80)
+
+class ObservationMedia(models.Model):
+    observation_id = models.ForeignKey(Observation, on_delete=models.CASCADE)
+    media_id       = models.ForeignKey(Media, on_delete=models.CASCADE)
+
+class Identification(models.Model):
+    source  = models.TextField()
+    species = models.TextField()
+
+class IdentificationObservation(models.Model):
+    observation_id    = models.ForeignKey(Observation, on_delete=models.CASCADE)
+    identification_id = models.ForeignKey(Identification, on_delete=models.CASCADE)
